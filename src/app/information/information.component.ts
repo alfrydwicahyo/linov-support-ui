09893830dc4453f29852332e86fc4bf144e1912f
Alfry <alfry.dwicahyo@gmail.com>
2019-07-10 09:02:11 +0700

    ganti password

--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,7 +1,8 @@
 import { Http } from '@angular/http';
 import { FormControl, FormGroup, FormArray, Validators, FormBuilder } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MenuItem } from 'primeng/primeng';
+import { WebStorageService, LOCAL_STORAGE } from 'angular-webstorage-service';
 
 @Component({
   selector: 'app-information',
@@ -15,12 +16,12 @@ export class InformationComponent implements OnInit {
   
   changeForm: FormGroup;
   private url = 'http://localhost:8181/user/';
-
-  constructor(private formBuilder: FormBuilder, private http: Http) { }
+  
+  constructor(private formBuilder: FormBuilder, private http: Http, @Inject(LOCAL_STORAGE) private storage: WebStorageService) { }
   
   ngOnInit() {
     this.breadCrumb();
-    
+    this.changePasswordForm();
   }
   
   breadCrumb() {
@@ -37,25 +38,35 @@ export class InformationComponent implements OnInit {
     })
   }
   
-  private prepareSave(): any {
-    let input = new FormData();
+  // private prepareSave(): any {
+  //   let input = new FormData();
     
-    input.append('oldPassword', this.changeForm.get('oldPassword').value),
-    input.append('newPassword', this.changeForm.get('newPassword').value)
+  //   input.append('oldPassword', this.changeForm.get('oldPassword').value),
+  //   input.append('newPassword', this.changeForm.get('newPassword').value)
     
-    console.log(input);
-    return input;
-  }
-
-  // onFormChange() {
-  //   const formModel = this.prepareSave();
-
-  //   let json = formModel;
-  //   console.log(json);
-
-  //   try {
-  //     this.http.patch(this.url + , json)
-  //   }
+  //   console.log(input);
+  //   return input;
   // }
+  
+  onFormChange() {
+    // const formModel = this.prepareSave();
+    
+    // let json = formModel;
+    // console.log(json);
+    
+    try {
+      this.http.patch(this.url + this.storage.get('id'), this.changeForm.value)
+      .subscribe
+      (res => {
+        console.log(res);
+      },
+      error => {
+        console.log(error);
+      })
+    } catch(e) {
+      console.log(e);
+    }
+    
+  }
   
 }
